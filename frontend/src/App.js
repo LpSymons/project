7008e10b6d7dec1dd86a6336ed982177fddcc9b4
Louis <71932780+LpSymons@users.noreply.github.com>
2023-03-10 17:07:43 +0000

    React App off the ground

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Shopping Project</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
